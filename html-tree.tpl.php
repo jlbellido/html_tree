f7618862c3729e1fb950fddee9af99a1f6b9ae81
javisr <jsantos@emergya.com>
2013-11-15 13:47:54 +0100

    initial commit

---
+++ html-tree.tpl.php
@@ -0,0 +1,6 @@
+<?php
+    global $base_url;
+?>
+<div id="html-tree">
+    <a href="<?php echo $base_url; ?>/html_tree/export" id="html-tree-link">EXPORT HTML TREE</a>
+</div>
---
+++ html_tree.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {
+    if (!$('#html-tree').length) {
+        var url = location.protocol +"//"+ location.host + Drupal.settings.basePath + 'html_tree/load';
+        var request = $.ajax({
+            url: url,
+            dataType: "json",
+            success: function(data){
+                $('body').append(data.output);
+                _html_tree_functionality();
+            }
+        });
+    }
+});
+
+function _html_tree_functionality(){
+    var link = $('#html-tree-link');
+    link.click(function(event){
+        event.preventDefault();
+        var url = location.protocol +"//"+ location.host + Drupal.settings.basePath + 'html_tree/export';
+
+        var data = {data: htmlTree()};
+        console.log(data);
+        var request = $.ajax({
+            type: 'post',
+            data: data,
+            url: url,
+            dataType: "json",
+            success: function(data){
+                window.location.href = data.redirect;
+            }
+        });
+
+
+
+    });
+}
+function htmlTree(obj){
+    var obj = obj || document.getElementsByTagName('body')[0];
+    child_classes =  _format_string(jQuery(obj).attr('class'),'class');
+    child_id = _format_string(jQuery(obj).attr('id'),'id');
+
+    var str = "<ul><li>" + obj.tagName;
+    str +=  ' ' + child_classes + ' ' + child_id;
+    if (obj.hasChildNodes()) {
+        var child = obj.firstChild;
+        while (child) {
+            if (child.nodeType === 1 && child.nodeName != 'SCRIPT'){
+                str += htmlTree(child)
+            }
+            child = child.nextSibling;
+        }
+    }
+
+    str += "</li></ul>\n";
+    return str;
+}
+function _format_string(string, attr){
+    var output = ''
+    if(string && attr){
+        output = attr + ': ' + string;
+    }
+    return output;
+}
+
